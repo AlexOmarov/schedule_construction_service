4761fcf96dce285285010cdc01ac26276f06b5dd
Alex Omarov <shtil.a@yandex.ru>
2023-06-09 13:15:05 +0700

    SCHED-12 Rsocket, Security
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

--- schedule_construction_app/src/main/kotlin/ru/shedlab/scheduleconstruction/domain/entity/Stub.kt
+++ schedule_construction_app/src/main/kotlin/ru/shedlab/scheduleconstruction/domain/entity/Stub.kt
@@ -4,14 +4,14 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.Transient
 import org.springframework.data.domain.Persistable
 import org.springframework.data.relational.core.mapping.Table
-import java.util.*
+import java.util.UUID
 
 @Table(value = "stub")
 data class Stub(
     @Id
     private var id: UUID,
     var name: String
-) : Persistable<UUID> {
+) : Persistable<UUID>, java.io.Serializable {
     @Transient
     var new: Boolean = true
 
